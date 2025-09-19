72334fd63bfdc3a2c6fc65478e61765d5fb34d9b
ziyang-c23 <ziyang-c23@mails.tsinghua.edu.cn>
2025-09-19 20:59:13 +0800

    modified:   astro.config.mjs 	new file:   src/assets/icons/bangumi.svg 	deleted:    src/assets/images/bangumi_icon.png 	modified:   src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -226,7 +226,7 @@ export const navBarConfig: NavBarConfig = {
 					name: "Bangumi",
 					url: "https://bangumi.tv/user/1150720",
 					external: true,
-					icon: "src/assets/images/bangumi_icon.png",
+					icon: "my:bangumi",
 				},
 				// {
 				// 	name: "Gitee",
@@ -304,7 +304,7 @@ export const profileConfig: ProfileConfig = {
 		},
 		{
 			name: "Bangumi",
-			icon: "src/assets/images/bangumi_icon.png",
+			icon: "my:bangumi",
 			url: "https://bangumi.tv/user/1150720",
 		},
 		// {
