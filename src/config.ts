989f48a574d504de3b13044a31d7bd5dfb47bf89
ziyang-c23 <ziyang-c23@mails.tsinghua.edu.cn>
2026-01-17 01:40:08 +0800

    animeList适量截断

--- src/config.ts
+++ src/config.ts
@@ -33,7 +33,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: _NAME,
 	subtitle: _SUBTITLE_1,
-	siteURL: "https://hako-blog-o4yzbraceu.edgeone.cool/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://hako-blog-esk0ujaf.edgeone.cool/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-09-22", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
